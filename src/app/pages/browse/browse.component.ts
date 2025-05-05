0ddccba69a2d1315d7c3696d272d982358de3b33
Kengni Johan <johan.kengni@campass.org>
2025-05-05 14:31:57 +0100

    Config tailwind css and init header component

---
+++ src/app/pages/browse/browse.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+import { HeaderComponent } from '../../core/components/header/header.component';
+
+@Component({
+  selector: 'app-browse',
+  imports: [HeaderComponent],
+  templateUrl: './browse.component.html',
+  styleUrl: './browse.component.css'
+})
+export class BrowserComponent {
+
+}
