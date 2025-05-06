fd072bb35c4099a7d84f667a8a5d5f506206dfed
Kengni Johan <johan.kengni@campass.org>
2025-05-06 11:40:51 +0100

    Implement header component

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  @Input({required: true}) userImg: string = '';
+  navList = ["Home", "TV Shows", "News & Popular", "My List", "Browse by Language"]
 }
